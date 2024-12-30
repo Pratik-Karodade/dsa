a58ebd4181d4d5650783e292b6263884676ed51f
Pratik-Karodade <karodadepratik@gmail.com>
2024-12-30 21:54:56 +0530

    print Linked list and append

---
+++ Linked List/src/com/company/LinkedList.java
@@ -0,0 +1,46 @@
+package com.company;
+
+public class LinkedList {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public void printLinkedList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void append(int value){
+        Node newNode = new Node(value);
+        if(length==0){
+            head=newNode;
+            tail=newNode;
+        }else {
+            tail.next=newNode; // yat apn tailcha value wala node newNode la connect krt ahe
+            tail=newNode; // yat apn newNode la tail lavat ahe
+        }
+        length++;
+    }
+
+    public void getHead() {
+        System.out.println("Head is : " + head.value);
+    }
+
+    public void getTail(){
+        System.out.println("Tail is : "+tail.value);
+    }
+
+    public void getLength(){
+        System.out.println("lenght : "+length);
+    }
+}
---
+++ Linked List/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList(4);
+        list.append(2);
+//        list.getHead();
+//        list.getTail();
+//        list.getLength();
+        list.printLinkedList();
+    }
+}
